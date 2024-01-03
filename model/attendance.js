71d951d185de0b1f75aed79eb27bcda4d648ad30
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-01-03 10:28:42 +0530

    Update attendance.js

--- model/attendance.js
+++ model/attendance.js
@@ -1,11 +1,21 @@
 const Sequelize = require('sequelize');
+
 const sequelize = require('../util/database');
 
-
-const Attendance = sequelize.define('attendance' , {
-    present: {
-        type: Sequelize.BOOLEAN
-    }
-})
+const Attendance = sequelize.define('Attendance', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  date: {
+    type: Sequelize.DATEONLY,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+});
 
 module.exports = Attendance;
